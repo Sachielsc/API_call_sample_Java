70a5558b7a8534be25f182f738cdd9a176719841
Charles Shu <cshu@planittesting.com>
2022-06-27 14:01:51 +1200

    1. disable parallel testing, i give up on log4j 2 2. add aftereach to testinheriprivate

--- src/test/java/com/charlessnippet/TestInheritPrivate.java
+++ src/test/java/com/charlessnippet/TestInheritPrivate.java
@@ -4,12 +4,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+
 import com.charlessnippet.demos.SubClass;
 import com.charlessnippet.demos.SuperClass;
 
 public class TestInheritPrivate
 {
     Logger logger = LogManager.getLogger(TestInheritPrivate.class);
+
+    @AfterEach
+    public void tearDown() {
+        logger.info("Closing the test ...");
+        logger.traceExit();
+    }
 
     @Test
     public void testInheritPrivate() {
