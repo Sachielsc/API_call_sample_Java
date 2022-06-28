b826acba28e62a2290769215cbf7548d2da6878c
Charles Shu <cshu@planittesting.com>
2022-06-28 19:52:12 +1200

    add method test_http_request_java11

--- src/test/java/com/charlessnippet/TestHttpRequest.java
+++ src/test/java/com/charlessnippet/TestHttpRequest.java
@@ -16,22 +16,26 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
-import java.sql.Connection;
-
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 // @Disabled("Disable as these are demo test cases")
 public class TestHttpRequest
 {
     Logger logger = LogManager.getLogger(TestHttpRequest.class);
     private HttpURLConnection httpURLConnection;
+    String siteURL = "https://jsonplaceholder.typicode.com/albums";
 
     @BeforeAll
     public static void setUp() {
@@ -40,6 +44,7 @@ public class TestHttpRequest
 
     @BeforeEach
     public void configureTools() {
+        
         logger.info("Start testing " + this.getClass().getSimpleName());
     }
 
@@ -50,18 +55,19 @@ public class TestHttpRequest
     }
     
     /**
-     * don't use this in your main thread
-     * this is not asynchronous
+     * <p>don't use this in your main thread</p>
+     * <p>this is not asynchronous</p>
      */
     @Test
     public void test_http_request()
     {
+        // String siteURL = "https://jsonplaceholder.typicode.com/albums";
         BufferedReader bufferedReader;
         String line;
         StringBuffer responseContent = new StringBuffer();
 
         try {
-            URL url = new URL("https://jsonplaceholder.typicode.com/albums");
+            URL url = new URL(siteURL);
             httpURLConnection = (HttpURLConnection) url.openConnection();
 
             // request setup
@@ -72,6 +78,20 @@ public class TestHttpRequest
             int status = httpURLConnection.getResponseCode();
             logger.info(status);
 
+            if (status > 299) {
+                bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+                while ((line = bufferedReader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                bufferedReader.close();
+            } else {
+                bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                while ((line = bufferedReader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                bufferedReader.close();
+            }
+            logger.info(responseContent.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -81,4 +101,16 @@ public class TestHttpRequest
         }
     }
 
+    /**
+     * <p>this is asynchronous</p>
+     */
+    @Test
+    public void test_http_request_java11() {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(siteURL)).build();
+        httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+        .thenApply(HttpResponse::body)
+        .thenAccept(logger::info)
+        .join();
+    }
 }
